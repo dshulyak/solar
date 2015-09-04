e9cb98a64a16ec9ccb481545a8aa4ff5e86b0499
Dmitry Shulyak <dshulyak@mirantis.com>
2015-09-04 12:04:54 +0300

    Add several improvements related to solar_resource
    - Remove celery installation from solar_resource
      transport supposed to be plugable, therefore we dont need to install solar everywhere
    - add -y for python-pip removal (ansible hangs without it)
    - add package for add-apt-repository util

--- bootstrap/playbooks/files/ubuntu-ansible.sh
+++ bootstrap/playbooks/files/ubuntu-ansible.sh
@@ -3,7 +3,7 @@
 # TODO: maybe this is better:
 # http://docs.ansible.com/ansible/intro_installation.html#latest-releases-via-apt-ubuntu
 
-apt-get remove -f python-pip
+apt-get remove -f -y python-pip
 sudo apt-get install -y python-setuptools
 sudo easy_install pip
 sudo pip install -U pip
