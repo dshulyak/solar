c991eb511f1fbf3e8e0bbd0fd4dbfdc858bcab5d
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-09 11:31:30 +0200

    Use config.py:C in solar and fix compose file

--- solar/solar/dblayer/__init__.py
+++ solar/solar/dblayer/__init__.py
@@ -1,6 +1,9 @@
 from solar.dblayer.model import ModelMeta
 from solar.dblayer.riak_client import RiakClient
-client = RiakClient(protocol='pbc', host='10.0.0.2', pb_port=8087)
+from solar.config import C
+
+client = RiakClient(
+    protocol=C.riak.protcol, host=C.riak.host, pb_port=C.riak.port)
 # client = RiakClient(protocol='http', host='10.0.0.2', http_port=8098)
 
 ModelMeta.setup(client)
