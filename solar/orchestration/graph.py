f4eb6b62e64f43ede451198d941b6c362950f8fe
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-18 13:55:30 +0200

    Add orch noop command to mark tasks as walked

--- solar/orchestration/graph.py
+++ solar/orchestration/graph.py
@@ -25,6 +25,7 @@ from collections import Counter
 
 from solar.dblayer.solar_models import Task
 from solar.dblayer.model import clear_cache
+from solar.dblayer.model import ModelMeta
 
 
 def save_graph(graph):
@@ -55,6 +56,14 @@ def update_graph(graph):
         task.errmsg = graph.node[n]['errmsg'] or ''
         task.save()
 
+def set_states(uid, tasks):
+    plan = get_graph(uid)
+    for t in tasks:
+        if t not in plan.node:
+            raise Exception("No task %s in plan %s", t, uid)
+        plan.node[t]['task'].status = states.NOOP.name
+        plan.node[t]['task'].save_lazy()
+    ModelMeta.save_all_lazy()
 
 def get_graph(uid):
     dg = nx.MultiDiGraph()
--- solar/orchestration/utils.py
+++ solar/orchestration/utils.py
@@ -26,7 +26,8 @@ def write_graph(plan):
         'ERROR': 'red',
         'SUCCESS': 'green',
         'INPROGRESS': 'yellow',
-        'SKIPPED': 'blue'}
+        'SKIPPED': 'blue',
+        'NOOP': 'black'}
 
     for n in plan:
         color = colors[plan.node[n]['status']]
